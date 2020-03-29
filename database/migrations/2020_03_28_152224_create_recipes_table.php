4e34bbccfb2a2f6afed52ae0b77f14d7cc9cd097
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-03-29 17:37:05 +0200

    Update tab formatting to 2 spaces

--- database/migrations/2020_03_28_152224_create_recipes_table.php
+++ database/migrations/2020_03_28_152224_create_recipes_table.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRecipesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('recipes', function (Blueprint $table) {
-            $table->id('id');
-            $table->string('url', 255);
-            $table->string('name', 255);
-            $table->string('image_url', 255)->nullable();
-            $table->text('description');
-            $table->float('rating_value');
-            $table->integer('rating_count');
-            $table->integer('servings');
-            $table->integer('total_time');
-            $table->text('ingredients');
-            $table->datetime('created_at');
-            $table->datetime('updated_at');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('recipes', function (Blueprint $table) {
+      $table->id('id');
+      $table->string('url', 255);
+      $table->string('name', 255);
+      $table->string('image_url', 255)->nullable();
+      $table->text('description');
+      $table->float('rating_value');
+      $table->integer('rating_count');
+      $table->integer('servings');
+      $table->integer('total_time');
+      $table->text('ingredients');
+      $table->datetime('created_at');
+      $table->datetime('updated_at');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('recipes');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('recipes');
+  }
 }
