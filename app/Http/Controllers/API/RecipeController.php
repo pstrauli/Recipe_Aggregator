77bbae96393bd9b6e4efcc33bb9d38a615bc5a76
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-03-30 14:32:17 +0200

    Exclude columns with 0 value from results

--- app/Http/Controllers/API/RecipeController.php
+++ app/Http/Controllers/API/RecipeController.php
@@ -8,9 +8,21 @@ use App\Http\Controllers\Controller;
 
 class RecipeController extends Controller
 {
+
+  // exclude columns with 0 value
+  protected $excluded_columns_with_possible_zero = [
+    ['total_time', '<>', '0'],
+    ['servings', '<>', '0'],
+    ['rating_count', '<>', '0'],
+
+  ];
+
   public function getMostRated()
   {
-    $recipes = Recipe::orderByDesc('rating_count')->take(10)->get();
+    $recipes = Recipe::where($this->excluded_columns_with_possible_zero)
+      ->orderBy('rating_count', 'desc')
+      ->limit(10)
+      ->get();
     return $recipes;
   }
 }
