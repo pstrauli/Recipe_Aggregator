b194f3b513208abeaca70e3501e045e26ba4fb9d
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-04-02 13:11:26 +0200

    Add more precise time fitlering

--- app/Http/Controllers/API/RecipeController.php
+++ app/Http/Controllers/API/RecipeController.php
@@ -32,21 +32,32 @@ class RecipeController extends Controller
 
     if (isset($request['time'])) {
       $time = $request['time'];
-      $this->where_statement[] = ['total_time', '<', "{$time}"];
+      if ($time == 30) {
+        $this->where_statement[] = ['total_time', '>', '1'];
+        $this->where_statement[] = ['total_time', '<=', '30'];
+      } elseif ($time == 60) {
+        $this->where_statement[] = ['total_time', '>', '30'];
+        $this->where_statement[] = ['total_time', '<=', '60'];
+      } elseif ($time == 120) {
+        $this->where_statement[] = ['total_time', '>', '60'];
+        $this->where_statement[] = ['total_time', '<=', '120'];
+      } elseif ($time == 240) {
+        $this->where_statement[] = ['total_time', '>', '120'];
+      } else {
+        $this->where_statement[] = ['total_time', '>', '1'];
+      }
     }
 
     if (isset($request['servings'])) {
       $servings = $request['servings'];
-      $this->where_statement[] = ['servings', '=', "{$servings}"];
+      $this->where_statement[] = ['servings', '>=', "{$servings}"];
     }
 
     if ($request['best'] == 1) {
-      $best = $request['best'];
       $this->where_statement[] = ['rating_value', '=', '5'];
     }
 
     if ($request['popular'] == 1) {
-      $popular = $request['popular'];
       $this->order_by = ['rating_count', 'desc'];
     }
 
